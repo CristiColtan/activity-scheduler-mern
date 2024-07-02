528c74b734e49f6b67babd9e65a3f353b4b05ffd
CristiColtan <cristicoltan0202@yahoo.com>
2024-07-02 13:48:34 +0300

    saving progress...

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const corsOptions = {
+  origin: true,
+  credentials: true,
+};
+
+const app = express();
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+
+app.listen(8081, () => {
+  console.log("Listening on port 8081...");
+});
+
+//define backend routes
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
