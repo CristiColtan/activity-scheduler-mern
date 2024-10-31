9fddda1e502b72ccb45c065b7dc396aab8d1a7b9
CristiColtan <cristicoltan0202@yahoo.com>
2024-10-31 08:57:28 +0200

    finished profile page design

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -13,6 +13,9 @@ import { getInitials } from '../utils/FullnameInitials.js';
 import { ImProfile } from "react-icons/im";
 import { MdRoomPreferences } from "react-icons/md";
 import { CiEdit } from "react-icons/ci";
+import { MdOutlineSecurity } from "react-icons/md";
+import { MdPrivacyTip } from "react-icons/md";
+import { MdMarkEmailUnread } from "react-icons/md";
 
 import { updateUserStart, updateUserFailure, updateUserSuccess } from "../redux/user/userSlice.js" 
 
@@ -23,7 +26,18 @@ const tabs = [{
 {
   title: "Preferences",
   icon: <MdRoomPreferences className='text-lg' size={20} />
-  
+},
+{
+  title: "Security",
+  icon: <MdOutlineSecurity className='text-lg' size={20} />
+},
+{
+  title: "Privacy",
+  icon: <MdPrivacyTip className='text-lg' size={20} />
+},
+{
+  title: "E-mail Notifications",
+  icon: <MdMarkEmailUnread className='text-lg' size={20} />
 }];
 
 const Profile = () => {
@@ -105,6 +119,8 @@ const Profile = () => {
   //e-mail, username, firstname, lastname daca exista si e gol sa il scoti din form data, 
   //title, role, city, gender daca exista si e gol sa fie trecut la Unknown
 
+  const [disabledPreferences, setDisabledPreferences] = useState(true);
+  
   return (
     <>
       <div className='w-full flex flex-col gap-3 mb-4 overflow-y-hidden'>
@@ -119,7 +135,7 @@ const Profile = () => {
                       <p className='font-serif'>Manage your CC Task experience</p>
                     </div>
                     <TabsProfile tabs={tabs} setSelected={setSelected}>
-                      {
+                      { //do flex items-center justify-center
                         selected === 0 && (
                           <>
                             <p className='font-serif text-2xl'>Personal Details</p>
@@ -133,12 +149,12 @@ const Profile = () => {
                               {/* 3 sections First Name */}
                               <div className='w-full grid grid-cols-3 gap-5 2xl:gap-8 overflow-y-auto border-t-2 mt-2'>
                                 {/* left */}
-                                <div className='space-y-6 p-4 bg-white shadow-lg text-center'>
+                                <div className='space-y-6 p-4 bg-white shadow-lg flex items-center justify-center text-center'>
                                   <span className='font-serif md:text-lg text-base'>First Name:</span>
                                 </div>
 
                                 {/* middle */}
-                                <div className='space-y-6 p-4 bg-white shadow-lg text-center'>
+                                <div className='space-y-6 p-4 bg-white shadow-lg flex items-center justify-center text-center'>
                                   <input type="text" placeholder={currentUser.first_name} required id="first_name" onChange={handleChange}
                                     value={formData.first_name || ''} disabled={!editableFields.first_name}
                                     className='border py-1 px-2 w-full placeholder-black focus:ring-2 ring-blue-300 outline-none
@@ -147,7 +163,7 @@ const Profile = () => {
                                 </div>
 
                                 {/* right */}
-                                <div className='space-y-6 p-4 bg-white shadow-lg  text-center'>
+                                <div className='space-y-6 p-4 bg-white shadow-lg text-center'>
                                   <button className={clsx('text-blue-700 hover:text-blue-400 inline-flex items-center disabled:cursor-not-allowed disabled:text-blue-500 border-gray-500 translate-y-1',
                                     { 'text-blue-400 shadow-xl border border-gray-200 rounded': editableFields.first_name }
                                   )}
@@ -386,12 +402,233 @@ const Profile = () => {
                             {error &&
                               (<p className='text-red-500 text-4xl'>Something went wrong! {error}</p>)}
                           </>
-                        )
-                      }
-                      {selected === 1 && (
-                        <div className='flex items-center justify-center'>
-                          bbb
-                        </div>
+                        )}
+                      {
+                        selected === 1 && (
+                          <>
+                            <p className='font-serif text-2xl'>Preferences</p>
+                            <p className='font-thin border-b-2 mb-4'>Change your language and accessibility
+                              requirements.
+                            </p>
+                          
+                            <div className='flex items-center justify-center border-t-2 flex-col mr-2'>
+                              <div className='w-full grid grid-cols-3 gap-5 2xl:gap-8 overflow-y-auto mt-2'>
+                                
+                                <div className='space-y-6 p-4 bg-white shadow-lg flex items-center justify-center text-center'>
+                                  <span className='font-serif md:text-lg text-base'>Language:</span>
+                                </div>
+
+                                <div className='space-y-6 p-4 bg-white shadow-lg flex items-center justify-center text-center'>
+                                  <input type="text" placeholder="American English"
+                                    disabled={true}
+                                    className='border py-1 px-2 w-full placeholder-black focus:ring-2 ring-blue-300 outline-none
+                                            border-black focus:placeholder-gray-500 rounded justify-center disabled:cursor-not-allowed'>
+                                  </input>
+                                </div>
+
+                                <div className='space-y-6 p-4 bg-white shadow-lg flex items-center justify-center text-center'>
+                                  <button className='text-blue-700 hover:text-blue-400 inline-flex 
+                                items-center disabled:cursor-not-allowed disabled:text-blue-500 border-gray-500'
+                                    disabled={true}>
+                                    {<CiEdit className='text-lg' size={26} />}
+                                  </button>
+                                </div>
+                      
+                              </div>
+
+                              <div className='w-full grid grid-cols-3 gap-5 2xl:gap-8 overflow-y-auto mt-2'>
+                                
+                                <div className='space-y-6 p-4 bg-white shadow-lg flex items-center justify-center text-center'>
+                                  <span className='font-serif md:text-lg text-base'>Accessibility requirements</span>
+                                </div>
+
+                                <div className='space-y-6 p-4 bg-white shadow-lg flex items-center justify-center text-center'>
+                                
+                                </div>
+
+                                <div className='space-y-6 p-4 bg-white shadow-lg flex items-center justify-center text-center'>
+                                  <button className='text-blue-700 hover:text-blue-400 inline-flex 
+                                items-center disabled:cursor-not-allowed disabled:text-blue-500 border-gray-500'
+                                    disabled={true}>
+                                    {<CiEdit className='text-lg' size={26} />}
+                                  </button>
+                                </div>
+                      
+                              </div>
+                            </div>
+                          </>
+                        )}
+                      {
+                        selected === 2 && (
+                        <>
+                          <p className='font-serif text-2xl'>Security</p>
+                            <p className='font-thin border-b-2 mb-4'>Change your security settings, set up secure
+                          authentication, or delete your account.
+                            </p>
+                          
+                            <div className='flex items-center justify-center border-t-2 flex-col mr-2'>
+                              <div className='w-full grid grid-cols-3 gap-5 2xl:gap-8 overflow-y-auto mt-2'>
+                                
+                                <div className='space-y-6 p-4 bg-white shadow-lg text-center flex items-center justify-center'>
+                                  <span className='font-serif md:text-lg text-base'>Password</span>
+                                </div>
+
+                                <div className='space-y-6 p-4 bg-white shadow-lg flex items-center justify-center text-center'>
+                                  <span className='font-serif md:text-base text-sm'>Reset your password regularly to keep your account
+                              secure</span>
+                                </div>
+
+                                <div className='space-y-6 p-4 bg-white shadow-lg flex items-center justify-center text-center'>
+                                  <button className='bg-blue-700 hover:bg-blue-400 inline-flex text-white font-medium 
+                                items-center disabled:cursor-not-allowed disabled:bg-blue-400 border-gray-500 px-4 py-2
+                                rounded'
+                                    disabled={true}>
+                                    Reset
+                                  </button>
+                                </div>
+                      
+                              </div>
+
+                              <div className='w-full grid grid-cols-3 gap-5 2xl:gap-8 overflow-y-auto mt-2'>
+                                
+                                <div className='space-y-6 p-4 bg-white shadow-lg flex items-center justify-center text-center'>
+                                  <span className='font-serif md:text-lg text-base'>Two-factor authentication</span>
+                                </div>
+
+                                <div className='space-y-6 p-4 bg-white shadow-lg text-center'>
+                                
+                                </div>
+
+                                <div className='space-y-6 p-4 bg-white shadow-lg flex items-center justify-center text-center'>
+                                  <button className='bg-blue-700 hover:bg-blue-400 inline-flex text-white font-medium 
+                                items-center disabled:cursor-not-allowed disabled:bg-blue-400 border-gray-500 px-3 py-2
+                                rounded'
+                                    disabled={true}>
+                                    Set up
+                                  </button>
+                                </div>
+                      
+                              </div>
+
+                              <div className='w-full grid grid-cols-3 gap-5 2xl:gap-8 overflow-y-auto mt-2'>
+                                
+                                <div className='space-y-6 p-4 bg-white shadow-lg flex items-center justify-center text-center'>
+                                  <span className='font-serif md:text-lg text-base'>Active sessions</span>
+                                </div>
+
+                                <div className='space-y-6 p-4 bg-white shadow-lg text-center'>
+                                  <span className='font-serif md:text-base text-sm'>
+                                    Selecting "Sign out" will sign you out from all
+                                    devices except this one.
+                                  </span>
+                                </div>
+
+                                <div className='space-y-6 p-4 bg-white shadow-lg flex items-center justify-center text-center'>
+                                  <button className='bg-red-700 hover:bg-red-400 inline-flex text-white font-medium 
+                                items-center disabled:cursor-not-allowed disabled:bg-red-400 border-gray-500 px-3 py-2
+                                rounded'
+                                    disabled={true}>
+                                    Sign out
+                                  </button>
+                                </div>
+                      
+                              </div>
+
+                              <div className='w-full grid grid-cols-3 gap-5 2xl:gap-8 overflow-y-auto mt-2'>
+                                
+                                <div className='space-y-6 p-4 bg-white shadow-lg flex items-center justify-center text-center'>
+                                  <span className='font-serif md:text-lg text-base'>Delete account</span>
+                                </div>
+
+                                <div className='space-y-6 p-4 bg-white shadow-lg text-center'>
+                                <span className='font-serif md:text-base text-sm'>
+                                    Permanently delete this account.
+                                  </span>
+                                </div>
+
+                                <div className='space-y-6 p-4 bg-white shadow-lg flex items-center justify-center text-center'>
+                                  <button className='bg-red-700 hover:bg-red-400 inline-flex text-white font-medium 
+                                items-center disabled:cursor-not-allowed disabled:bg-red-400 border-gray-500 px-4 py-2
+                                rounded'
+                                    disabled={true}>
+                                    Delete
+                                  </button>
+                                </div>
+                      
+                              </div>
+                            </div>
+                        </>
+                        )}
+                      {
+                        selected === 3 && (
+                          <>
+                            <p className='font-serif text-2xl'>Privacy</p>
+                            <p className='font-thin border-b-2 mb-4'>Exercise your privacy rights and control how your data
+                          is used.
+                            </p>
+                          
+                            <div className='flex items-center justify-center border-t-2 flex-col mr-2'>
+                              <div className='w-full grid grid-cols-3 gap-5 2xl:gap-8 overflow-y-auto mt-2'>
+                                
+                                <div className='space-y-6 p-4 bg-white shadow-lg text-center flex items-center justify-center'>
+                                  <span className='font-serif md:text-lg text-base'>Privacy settings</span>
+                                </div>
+
+                                <div className='space-y-6 p-4 bg-white shadow-lg flex items-center justify-center text-center'>
+                                  <span className='font-serif md:text-base text-sm'>
+                                    Select "Manage" to change your privacy
+                              settings and exercise your rights using our
+                              request form.</span>
+                                </div>
+
+                                <div className='space-y-6 p-4 bg-white shadow-lg flex items-center justify-center text-center'>
+                                  <button className='bg-blue-700 hover:bg-blue-400 inline-flex text-white font-medium 
+                                items-center disabled:cursor-not-allowed disabled:bg-blue-400 border-gray-500 px-3 py-2
+                                rounded'
+                                    disabled={true}>
+                                    Manage
+                                  </button>
+                                </div>
+                      
+                              </div>
+                            </div>
+                          </>
+                        )}
+                      {
+                        selected === 4 && (
+                          <>
+                            <p className='font-serif text-2xl'>E-mail notifications</p>
+                            <p className='font-thin border-b-2 mb-4'>Decide what you wantto be notified about, and
+                          unsubscribe from what you don't.
+                            </p>
+                          
+                            <div className='flex items-center justify-center border-t-2 flex-col mr-2'>
+                              <div className='w-full grid grid-cols-3 gap-5 2xl:gap-8 overflow-y-auto mt-2'>
+                                
+                                <div className='space-y-6 p-4 bg-white shadow-lg text-center flex items-center justify-center'>
+                                  <span className='font-serif md:text-lg text-base'>E-mail preferences</span>
+                                </div>
+
+                                <div className='space-y-6 p-4 bg-white shadow-lg flex items-center justify-center text-center'>
+                                  
+                                  <span >
+                                    <span className='font-thin'>{currentUser.email}</span>
+                                    <br></br>
+                                    <span className='font-serif md:text-base text-sm'>This is the email address we send you notifications.</span>
+                                  </span>
+                                </div>
+
+                                <div className='space-y-6 p-4 bg-white shadow-lg flex items-center justify-center text-center'>
+                                  <button className='text-blue-700 hover:text-blue-400 inline-flex 
+                                items-center disabled:cursor-not-allowed disabled:text-blue-500 border-gray-500'
+                                    disabled={true}>
+                                    {<CiEdit className='text-lg' size={26} />}
+                                  </button>
+                                </div>
+                      
+                              </div>
+                            </div>
+                          </>
                       )}
                     </TabsProfile>
                     <div className='w-full flex flex-col md:flex-row gap-5 2xl:gap-8 overflow-y-auto'>
